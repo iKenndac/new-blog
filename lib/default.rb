0f5189c110b0b476d4f7b32c2ce76e09bb750842
Daniel Kennett <github@ikennd.ac>
2015-02-22 11:20:29 +0100

    Add previous/next links to the bottom of post pages

--- lib/default.rb
+++ lib/default.rb
@@ -40,6 +40,30 @@ module PostHelper
     end.sort.reverse
   end
 
+  def previous_link
+    prev = sorted_articles.index(@item) + 1
+    prev_article = sorted_articles[prev]
+    if prev_article.nil?
+      ''
+    else
+      title = prev_article[:title]
+      html = "&larr; Previous"
+      link_to(html, prev_article.reps[0], :class => "previous", :title => title)
+    end
+  end
+
+  def next_link
+    nxt = sorted_articles.index(@item) - 1
+    if nxt < 0
+      ''
+    else
+      post = sorted_articles[nxt]
+      title = post[:title]
+      html = "Next &rarr;"
+      link_to(html, post.reps[0], :class => "next", :title => title)
+    end
+  end
+
 end
 
 
