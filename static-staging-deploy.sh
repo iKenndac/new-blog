5f3b4184cde161e3474d5370f182f7ba9697e5f4
Daniel Kennett <daniel@cascable.se>
2019-02-01 16:47:56 +0100

    Add static-staging support

---
+++ static-staging-deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -d "output" ]; then
+    echo "No output directory!"
+    exit 1
+fi
+
+BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
+
+SANITIZED_BRANCH_NAME=`echo "${BRANCH_NAME}" | tr A-Z a-z | sed -e 's/[^a-zA-Z0-9\-]/-/g'`
+SANITIZED_BRANCH_NAME=`echo "${SANITIZED_BRANCH_NAME}" | sed 's/\(--*\)/-/g'`
+DEPLOY_DIRECTORY_NAME="${SANITIZED_BRANCH_NAME}.static-staging.ikennd"
+
+ME=`whoami`
+echo "Deploying ${BRANCH_NAME} to ${DEPLOY_DIRECTORY_NAME}.ac for ${ME}…"
+
+rsync -r --links --safe-links output/ "website_deployment@static-staging.ikennd.ac:/ikenndac/public_html/content/${DEPLOY_DIRECTORY_NAME}/"
